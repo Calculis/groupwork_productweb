318885706a1ad253943ed9f424afa60cf53ad6b7
zomqs <68818649+zomqs@users.noreply.github.com>
2021-10-28 17:10:45 +0700

    3-7

--- products.js
+++ products.js
@@ -14,6 +14,41 @@ let products =[
         quantity : 25,
         desc : "LIGHTSPEED เกมมิ่งเมาส์"
     },{
+        id : 3,
+        img : "g604.png",
+        name : "G604",
+        price : 3299,
+        quantity : 35,
+        desc : "LIGHTSPEED Wireless Gaming Mouse"
+    },{
+        id : 4,
+        img : "g302.png",
+        name : "G302 DAEDALUS PRIME",
+        price : 1349,
+        quantity : 42,
+        desc : "MOBA GAMING MOUSE"
+    },{
+        id : 5,
+        img : "g512.png",
+        name : "G512 CARBON",
+        price : 4499,
+        quantity : 22,
+        desc : "RGB Mechanical Gaming Keyboard"
+    },{
+        id : 6,
+        img : "g413.png",
+        name : "G413",
+        price : 3299,
+        quantity : 56,
+        desc : "Mechanical Backlit Gaming Keyboard"
+    },{
+        id : 7,
+        img : "g913.png",
+        name : "G913",
+        price : 8999,
+        quantity : 15,
+        desc : "คีย์บอร์ดเกมมิ่งเชิงกล LIGHTSPEED Wireless RGB"
+    },{
         id: 8,
         img: "./img/",
         name : "Nubwo NBCH X107",
