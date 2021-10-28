5c49b38e20c8ae7004c44665366868180f991a6d
neko7687 <50837502+neko7687@users.noreply.github.com>
2021-10-28 16:41:46 +0700

    Productlist

---
+++ productlist.js
@@ -0,0 +1,17 @@
+export let product =[
+    {   
+        id : 1,
+        img : "g403.jpg",
+        name : "G403",
+        price : 2699,
+        quantity : 50,
+        desc : "เมาส์เกมมิ่ง HERO"
+    },{
+        id : 2,
+        img : "g502.jpg",
+        name : "G502",
+        price : 5999,
+        quantity : 25,
+        desc : "LIGHTSPEED เกมมิ่งเมาส์"
+    }
+]
