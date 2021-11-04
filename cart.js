698fddd28a0a3bdb9783dd621be069342a7ba122
Phunon Srivoraluxkana <phunon.sriv@mail.kmutt.ac.th>
2021-11-04 23:46:09 +0700

    finish DOM cart

--- cart.js
+++ cart.js
@@ -1,9 +1,29 @@
 // set add to cart button
 const addBtn=document.getElementsByClassName("add")
+const cart=[]
+const allChild=main.childNodes
+for(let i=0;i<allChild.length;i++){
+    const id=allChild[i].firstChild.nextSibling.id
+    const bt=allChild[i].lastChild;
+    bt.addEventListener("click",()=>{
+        if(cart_duplicate(id)==false){
+            cart.unshift({"id":id,quantity:1})
+           
+        }
+        else{
+           const target= cart.find( item=> item["id"]==id )
+           target["quantity"]+=1
+        }
 
-const addToCart=(event,p1)=>{
-   console.log("Hello");
+    })
 }
 
-
+const cart_duplicate=(id)=>{
+  for(let i=0;i<cart.length;i++){
+      if(cart[i].id==id){
+          return true
+      }
+  }
+  return false
+}
 
