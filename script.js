a12b19d38e21c46504cb9bb779ccd3ccd3ff9b0c
warisara <warisara.khrua@mail.kmutt.ac.th>
2021-11-06 14:35:13 +0700

    ฺContent edit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ export const main =document.getElementById("main")
 import { set_cart_btn } from "./cart.js"
 import { products } from "./products.js"
 var count=1
-export const header_txt=document.getElementById("header_txt");
 export function load_mainpage(products){
 for(const p of products){
     
@@ -23,23 +22,26 @@ for(const p of products){
         else if(l == "name"){
             const text = document.createElement("p")
             text.setAttribute("id",p[l])
-            text.textContent= `${l}:${p[l]}`
-            text.setAttribute("class","hover:text-white hover:bg-indigo-300 px-4")
+            // text.textContent= `${l}:${p[l]}`
+            text.textContent= `${p[l]}`
+            text.setAttribute("class","bg-white text-xl text-center text-indigo-500 rounded-t-xl hover:text-white hover:bg-indigo-300 pt-2 pb-2 px-4")
             container.appendChild(text)
 
         }
         else if(l == "id"){
             const text = document.createElement("p")
             text.setAttribute("id",p[l])
-            text.textContent= `${l}:${p[l]}`
+            // text.textContent= `${l}:${p[l]}`
             text.setAttribute("class","hover:text-white hover:bg-indigo-300 px-4")
             container.appendChild(text)
 
         }
         else{
+        //price quan desc
         const text = document.createElement("p")
-        text.setAttribute("class","hover:text-white hover:bg-indigo-300 px-4")
+        text.setAttribute("class","text-center bg-white rounded-b-xl hover:text-white hover:bg-indigo-300 px-4")
         text.textContent= `${l}:${p[l]}`
+        // text.textContent= `${p[l]}`
         container.appendChild(text)
         // const text= 
         // console.log(text);
@@ -50,7 +52,7 @@ for(const p of products){
     addBtn.className="add"
     addBtn.setAttribute(
         "class",
-        "mt-12 w-full bg-indigo-400 transition duration-200 rounded-md py-3 px-3 flex items-center justify-center text-white hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        "mt-4 w-full bg-indigo-400 transition duration-200 rounded-md py-3 px-3 flex items-center justify-center text-white hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       );
     addBtn.textContent="Add to cart"
     container.appendChild(addBtn)
@@ -59,7 +61,5 @@ for(const p of products){
     
 }
     set_cart_btn()
-    header_txt.textContent="All Product"
-
 }
 load_mainpage(products)
