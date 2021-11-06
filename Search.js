5847cb98dc1fc6b033df53e0510440c0fb8864a0
Teeradet Panklai <teeradet.bigkung@mail.kmutt.ac.th>
2021-11-06 13:08:21 +0700

    Search

--- Search.js
+++ Search.js
@@ -2,32 +2,39 @@ import { products } from "./products.js"
 
 const productList = document.querySelector('#main');
 
-let inputName = document.filter(Name => Name.name.includes(""));
+// let inputName = productList.filter(Name => Name.name.includes(""));
 
-let searchbt = document.getElementById('sbt')
+let searchbt = document.getElementById('Search')
+let sbt = document.getElementById('sbt')
 
 searchbt.addEventListener('click', () =>{
     SearchButton();
     },false)
 
 function SearchButton(){
-    let s = document.getElementById('sbt').value.toLowerCase();
-    inputName = products.filter(Name => Name.name.toLowerCase().includes(`${s}`));
-    console.log(input.length == 0);
-    let chilProduct = productList.children;
+    var inputName = sbt.value;
+    console.log(inputName.length == 0);
+    let chilProduct = productList.childNodes;
     if(inputName.length == 0){
-        for(let a of chilProduct){
-            a.style.display = 'none';
-        }
     }else{
-        for(let a of chilProduct){
-            for(let b of inputName){
-                if(a.id == b.id){   
-                    a.style.display = '';
-                    break;
-                }else{
-                    a.style.display = 'none';
-                }
+        // for(let a of chilProduct){
+        //     for(let b of inputName){
+        //         if(a.id == b.id){   
+        //             a.style.display = '';
+        //             break;
+        //         }else{
+        //             a.style.display = 'none';
+        //         }
+        //     }
+        // }
+
+        for(let i = 0; chilProduct.length > i; i++){
+            let productName = products[i].name
+            if(productName == inputName){ 
+            }else{
+                let productId = document.getElementById(productName)
+                let productParent = productId.parentNode
+                productParent.style.display = 'none';
             }
         }
     }
