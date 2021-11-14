fbc30d3f2a5c3bdeb450807187b885351d151b09
Phunon Srivoraluxkana <phunon.sriv@mail.kmutt.ac.th>
2021-11-14 17:37:30 +0700

    set cookie

--- cookie.js
+++ cookie.js
@@ -2,10 +2,32 @@ import {cart} from "./cart.js"
 
 const items=cart
 function setCookie(){
-    for(let i=0;i<items.length;i++){
-        const cookieText=encodeURIComponent(`name=${"item"+(i+1)} ;value=id:${items[i].id},quantity:${items[i].quantity};max-age=${60*60*24}`)
-        document.cookie=cookieText
-    }
+    let count=1
+    clearCartCookies()
+for (const iterator of items) {
+    
+    const txt= (`${encodeURIComponent('item'+count)}=${iterator.id}|${iterator.quantity};max-age=${60*60*24}`)
+    document.cookie=txt
+    count++
 
+    // console.log(iterator.id);
+    // console.log(iterator.quantity);
+    
 }
+
+    }
+   
+
+function clearCartCookies(){
+    const c=document.cookie
+    const cookie=c.split(";")
+    cookie.forEach(c=>{
+        const eqPos=c.indexOf("=")
+        document.cookie=c.substring(0,eqPos+1)+";expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+
+    })
+}
+
+
 window.onbeforeunload=setCookie
+
